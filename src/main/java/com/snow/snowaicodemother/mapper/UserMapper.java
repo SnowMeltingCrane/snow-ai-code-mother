d9821ebbd89239402ace3eb99696e8a47b57958a
SnowCrane <1277456806@qq.com>
2025-09-01 00:09:04 +0800

    feat: 添加用户相关功能和接口

---
+++ src/main/java/com/snow/snowaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.snow.snowaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.snow.snowaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/SnowMeltingCrane">雪融鹤</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
