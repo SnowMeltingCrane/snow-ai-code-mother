37828d363801816a3b6eb2c58a387ad99544b31e
SnowCrane <1277456806@qq.com>
2025-08-31 18:46:10 +0800

    项目初始化

---
+++ src/main/java/com/snow/snowaicodemother/SnowAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.snow.snowaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snow/snowaicodemother/SnowAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snow.snowaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
